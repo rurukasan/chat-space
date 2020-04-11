fecfc9fa3e41e5df7fe015f5254fc9d68d789415
rurukasan <ruka20020212@icloud.com>
2020-04-11 16:33:28 +0900

    AWS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifiercd  /var/www/chat-spaceless log/unicorn.stderr.log
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
