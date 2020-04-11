090ca820b33f3f884a5a52e3e8e0cd83a93fd303
rurukasan <ruka20020212@icloud.com>
2020-04-11 17:52:18 +0900

    AWS

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,16 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+# ../が一つ増えている
+app_path = File.expand_path('../../../', __FILE__)
+
+worker_processes 1
+# currentを指定
+working_directory "#{app_path}/current"
+
+# それぞれ、sharedの中を参照するよう変更
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1
@@ -27,7 +38,7 @@ timeout 60
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
 
-check_client_connection false
+check_client_connection falsekill <確認したunicorn rails masterのPID（上記では17877）>
 
 run_once = true
 
